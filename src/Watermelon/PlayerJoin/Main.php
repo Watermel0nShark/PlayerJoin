277a3e2ef3417ac8c4c29f3ed6dff0de0b75a186
Watermel0nShark <68258021+Watermel0nShark@users.noreply.github.com>
2020-09-17 07:13:49 -0500

    Update Main.php

--- src/Watermelon/PlayerJoin/Main.php
+++ src/Watermelon/PlayerJoin/Main.php
@@ -5,12 +5,13 @@ namespace Watermelon\PlayerJoin;
 use pocketmine\Server;
 use pocketmine\Player;
 
-use pocketmine\plugin\PluginBase as pl;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 use pocketmine\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 
-class main extends pl implements Listener {
+class main extends PluginBase implements Listener {
    
    public function onEnable(){
       $this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -18,6 +19,10 @@ class main extends pl implements Listener {
       $this->saveDefaultConfig();
       $this->getResource("config.yml");
    }
+    
+   public function onLoad {
+      $this->getLogger()->info ("plugin has loaded")
+}
    
    public function onJoin(PlayerJoinEvent $event){
       $player = $event->getPlayer();
